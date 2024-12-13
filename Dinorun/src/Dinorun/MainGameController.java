dcce62878a45d0c038f2fac5c60dfeddbe2787ee
nurulaisyah19 <151292391+nurulaisyah19@users.noreply.github.com>
2024-12-13 21:37:56 +0700

    Update MainGameController.java
    nambahin atribut dan variabel

--- Dinorun/src/Dinorun/MainGameController.java
+++ Dinorun/src/Dinorun/MainGameController.java
@@ -19,3 +19,53 @@ import javafx.scene.layout.Pane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+public class MainGameController {
+   @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Pane gameArea;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private ImageView dino;
+
+    @FXML
+    private Label scoreLabel;
+
+    @FXML
+    private Button restartButton;
+
+    @FXML
+    private Button balikMenu;
+
+    private ImageView kayu, batu; 
+    private int score = 0;
+    private double obstacleSpeed = 5.0;
+    private boolean isGameRunning = true;
+    boolean isDown = false;
+
+    private double velocityY = 0;
+    private final double GRAVITY = 0.1;
+
+    
+    private final Image kayuImage = new Image(getClass().getResource("/resource/kayu.png").toExternalForm());
+    private final Image batuImage = new Image(getClass().getResource("/resource/batu.png").toExternalForm());
+    private final Random random = new Random();
+
+    private long lastUpdateTime = 0; 
+    private long lastSpawnTime = 0;
+    private static final long SPEED_INCREASE_INTERVAL = 1000000000L; 
+    private static final long OBSTACLE_SPAWN_INTERVAL = 1500000000L; 
+        
+    Dir dir = Dir.botbot; 
+    Message message = new Message();
+    AlertAlert alertalert = new AlertAlert();
+    
+
+    enum Dir{
+        upup,botbot;
+    }
