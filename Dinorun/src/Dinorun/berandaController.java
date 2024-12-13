bf99a0bee4c825a49fbea84264fe7c329ba6c289
lekok indah lia <144763130+lillia05@users.noreply.github.com>
2024-12-13 21:33:54 +0700

    Update berandaController.java

--- Dinorun/src/Dinorun/berandaController.java
+++ Dinorun/src/Dinorun/berandaController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package game;
+package Dinorun;
 
 import java.io.IOException;
 import java.net.URL;
