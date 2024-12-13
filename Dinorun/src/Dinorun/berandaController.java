f03ec75827ca86ad1ed53211b06dda9fd4b8924a
lekok indah lia <144763130+lillia05@users.noreply.github.com>
2024-12-13 22:23:02 +0700

    Update berandaController.java
    mengedit button start

--- Dinorun/src/Dinorun/berandaController.java
+++ Dinorun/src/Dinorun/berandaController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
- */
 package Dinorun;
 
 import java.io.IOException;
@@ -10,33 +6,39 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-//import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-//import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-//import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-/**
- *
- * @author User
- */
 public class berandaController implements Initializable {
-    
-
     @FXML
     private Button button;
     private Parent root;
     private Stage stage;
     private Scene scene;
-    
   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    
+        button.setOnMouseEntered(event -> button.setStyle(
+            "-fx-background-color: #65962c; " +  
+            "-fx-text-fill: white; " + 
+            "-fx-font-size: 20px; " +
+            "-fx-font-weight: bold; " +
+            "-fx-padding: 10 20 10 20; " +
+            "-fx-background-radius: 100;"
+        ));
+
+        button.setOnMouseExited(event -> button.setStyle(
+            "-fx-background-color: #80cc28; " +  
+            "-fx-text-fill: #9b6736; " +
+            "-fx-font-size: 20px; " +
+            "-fx-font-weight: bold; " +
+            "-fx-padding: 10 20 10 20; " +
+            "-fx-background-radius: 100;"
+        ));
     }    
     
      @FXML
